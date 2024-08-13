a4f89db7b074561ee763b92b4880357ec82f95bc
sajinayonjan444 <161477863+sajinayonjan444@users.noreply.github.com>
2024-08-13 05:03:23 -0700

    Add files via upload

---
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,12 @@
+import styles from "./ContactHeader.module.css";
+const ContactHeader = () => {
+  return (
+    <div className={styles.contact_section} container>
+        <h1>Contact Us</h1>
+        <p>LET'S CONNECT:WE'RE HERE TO HELP,WE WOULD LOVE TO HEAR FROM YOU!WHETHER YOU HAVE A QUESTION,COMMENT OR JUST WANT TO CHAT,YOU CAN REACH OUT TO US THROUGH THIS CONTACT PAGE OR BY PHONE OR BY EMAIL OR SOCIAL MEDIA</p>
+
+    </div>
+  );
+};
+
+export default ContactHeader;
